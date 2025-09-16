6e1ca60e86419d8d5da5b6cdf27c33857ed2a60e
dophuochung2428 <dophuochung2428@gmail.com>
2025-09-17 01:16:05 +0700

    feat(accounts): add bulk delete with confirmation dialog

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,10 +2,11 @@ import { Hono } from "hono";
 
 import { db } from "@/db/drizzle";
 import { zValidator } from "@hono/zod-validator";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { createId } from "@paralleldrive/cuid2"
 import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import z from "zod";
 
 const app = new Hono()
     .get(
@@ -37,10 +38,10 @@ const app = new Hono()
         })),
         async (c) => {
             const auth = getAuth(c);
-            const values =c.req.valid("json");
+            const values = c.req.valid("json");
 
             if (!auth?.userId) {
-                return c.json({ error: "Unauthorized"}, 401);
+                return c.json({ error: "Unauthorized" }, 401);
             }
 
             const [data] = await db.insert(accounts).values({
@@ -51,5 +52,37 @@ const app = new Hono()
 
             return c.json({ data })
         })
+    .post(
+        "/bulk-delete",
+        clerkMiddleware(),
+        zValidator(
+            "json",
+            z.object({
+                ids: z.array(z.string()),
+            }),
+        ),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
+
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const data = await db
+                .delete(accounts)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        inArray(accounts.id, values.ids)
+                    )
+                )
+                .returning({
+                    id: accounts.id,
+                });
+
+            return c.json({ data });
+        },
+    );
 
 export default app;
